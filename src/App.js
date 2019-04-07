cb631d459e9ccad80b340f9c908e00145b4823f1
unknown <mistrzuz122@gmail.com>
2019-04-07 22:33:51 +0200

    dodatnie komponentu Particles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,51 @@ import './App.css';
 // import Loading from './Components/Loading/Loading';
 import Navigation from './Components/Navigation/Navigation';
 import 'tachyons';
+import Particles from 'react-particles-js';
+
+const params = {
+  "particles": {
+      "number": {
+          "value": 60,
+          "density": {
+              "enable": true,
+              "value_area": 1500
+          }
+      },
+      "line_linked": {
+          "enable": true,
+          "opacity": 0.02
+      },
+      "move": {
+          "direction": "right",
+          "speed": 0.05
+      },
+      "size": {
+          "value": 1
+      },
+      "opacity": {
+          "anim": {
+              "enable": true,
+              "speed": 1,
+              "opacity_min": 0.05
+          }
+      }
+  },
+  "interactivity": {
+      "events": {
+          "onclick": {
+              "enable": true,
+              "mode": "push"
+          }
+      },
+      "modes": {
+          "push": {
+              "particles_nb": 1
+          }
+      }
+  },
+  "retina_detect": true
+}
 
 class App extends Component {
   constructor(props) {
@@ -15,6 +60,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles params={params} className="particles"/>
         <Navigation />
         {/* <Loading /> */}
       </div>
