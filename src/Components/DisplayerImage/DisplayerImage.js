f1d602e020b1c20379a00e4e87ca64a68199e21c
unknown <mistrzuz122@gmail.com>
2019-04-09 02:22:07 +0200

    update mały

--- src/Components/DisplayerImage/DisplayerImage.js
+++ src/Components/DisplayerImage/DisplayerImage.js
@@ -4,34 +4,34 @@ import './DisplayerImage.css';
 
 const DisplayerImage = ({ url, predict }) => {
     return (
-        <div className="flex flex-wrap">
-            <div className="eldo center pa3 ma2 br3 shadow-5">
-                <img
-                    width="700px"
-                    height="auto"
-                    id="image"
-                    alt=""
-                    src={url}
-                />
+        <div className="flex flex-wrap justify-around mh5">
+            <div>
+                <div className="eldo center pa3 ma3 br3 shadow-5">
+                    <img
+                        id="image"
+                        alt=""
+                        src={url}
+                    />
+                </div>
             </div>
-            <div className="serdecznie center pa3 ma2 br3 shadow-5">
-            <h2>Przypuszczenia:</h2>
-            {
-                predict.map((name, i) => {
-                    return (
-                        <Predictions
-                            key={i}
-                            name={name.name}
-                            value={name.value}
-                        />
-                    )
-                })
-            }
+            <div>
+                <div className="serdecznie center pa3 ma3 br3 shadow-5 tl">
+                    <h2>Przypuszczenia:</h2>
+                    {
+                        predict.map((name, i) => {
+                            return (
+                                <Predictions
+                                    key={i}
+                                    name={name.name}
+                                    value={name.value}
+                                />
+                            )
+                        })
+                    }
+                </div>
             </div>
         </div>
     )
 }
-
-// https://i.kinja-img.com/gawker-media/image/upload/s--HqfzgkTd--/c_scale,f_auto,fl_progressive,q_80,w_800/wp2qinp6fu0d8guhex9v.jpg
 
 export default DisplayerImage;
